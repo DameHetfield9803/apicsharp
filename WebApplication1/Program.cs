f3732392a25e9945017c3b541f2273fc3eaf4e14
DameHetfield9803 <you@example.com>
2024-05-30 21:14:23 +0800

    Add project files.

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,35 @@
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+});
+
+var app = builder.Build();
+
+var sampleTodos = new Todo[] {
+    new(1, "Kill yourself"),
+    new(2, "Kill yourself", DateOnly.FromDateTime(DateTime.Now)),
+    new(3, "Kill yourself", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
+    new(4, "Kill yourself"),
+    new(5, "Kill yourself", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
+};
+
+var todosApi = app.MapGroup("/todos");
+todosApi.MapGet("/", () => sampleTodos);
+todosApi.MapGet("/{id}", (int id) =>
+    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+        ? Results.Ok(todo)
+        : Results.NotFound());
+
+app.Run();
+
+public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
+
+[JsonSerializable(typeof(Todo[]))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext
+{
+
+}
